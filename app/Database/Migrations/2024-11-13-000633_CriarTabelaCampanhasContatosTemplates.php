b720f94c660fdb2b718ab979cff3a081a57315f8
Ederson Fernandes <edersontec@outlook.com>
2024-11-12 21:52:59 -0300

    db: Desenvolvimento de migration para criar tabela campanhas_contatos_templates

---
+++ app/Database/Migrations/2024-11-13-000633_CriarTabelaCampanhasContatosTemplates.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CriarTabelaCampanhasContatosTemplates extends Migration
+{
+    public function up()
+    {  
+
+        $fields = [
+            'id' => [
+                'type' => 'INTEGER',
+                //'constraint' => 5,
+                'unique' => true,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'campanhas_id' => [
+                'type' => 'INTEGER',
+                //'constraint' => 5,
+                'unsigned' => true,
+            ],
+            'contatos_id' => [
+                'type' => 'INTEGER',
+                //'constraint' => 5,
+                'unsigned' => true,
+            ],
+            'templates_id' => [
+                'type' => 'INTEGER',
+                //'constraint' => 5,
+                'unsigned' => true,
+            ]
+        ];
+
+        $this->forge->addField($fields);
+
+        $this->forge->addPrimaryKey('id');
+
+        $this->forge->addForeignKey('campanhas_id', 'campanhas', 'id');
+        $this->forge->addForeignKey('contatos_id', 'contatos', 'id');
+        $this->forge->addForeignKey('templates_id', 'templates', 'id');
+
+        $this->forge->createTable('campanhas_contatos_templates');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('campanhas_contatos_templates');
+    }
+}
+
